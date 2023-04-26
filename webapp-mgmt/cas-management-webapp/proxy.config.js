f0424ac59e859bb693fa4b2e9bc8d3b3b244dbbf
duqian <duqian42707@163.com>
2023-04-26 23:40:03 +0800

    准备Angular前端页面翻译

---
+++ webapp-mgmt/cas-management-webapp/proxy.config.js
@@ -0,0 +1,26 @@
+const proxy = {
+  target: "http://localhost:8080",
+  changeOrigin: true,
+  secure: false
+}
+// const prefix = "";
+const prefix = "/backstage/cas-management";
+const apis = [
+  '/appConfig',
+  '/user',
+  '/footer',
+  '/images',
+  '/gitStatus',
+  '/getService',
+  '/getServices',
+  '/formData',
+]
+
+const data = {};
+
+for (let i = 0; i < apis.length; i++) {
+  const path = prefix + apis[i]
+  data[path] = proxy;
+}
+
+module.exports = data;
